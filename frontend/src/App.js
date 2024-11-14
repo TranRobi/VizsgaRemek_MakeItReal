d560fc8329035d80c3bd3db0ad6c0a2474bfd1da
TranRobi <dtranduy@gmail.com>
2024-11-14 08:35:06 +0100

    added links to footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,23 +7,25 @@ import Library from "./Pages/Library/Library.js";
 import Register from "./Pages/Register/Register.js";
 import Order from "./Pages/Order/Order.js";
 import ContactUs from "./Pages/ContactUs/ContactUs.js";
+import AboutUs from "./Pages/AboutUs/AboutUs.js";
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <div>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/library" element={<Library />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/order" element={<Order />} />
-            <Route path="/contactus" element={<ContactUs />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </div>
-  );
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<div>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/library" element={<Library />} />
+						<Route path="/login" element={<Login />} />
+						<Route path="/register" element={<Register />} />
+						<Route path="/order" element={<Order />} />
+						<Route path="/contactus" element={<ContactUs />} />
+						<Route path="/aboutus" element={<AboutUs />} />
+					</Routes>
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
