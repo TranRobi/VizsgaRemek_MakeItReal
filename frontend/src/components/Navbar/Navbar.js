e790c1431c7eaaa8e94ffc21b7b3778b502b9c77
Tran Duy Dat <dtranduy2@gmail.com>
2024-11-14 15:49:40 +0100

    active link added

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,41 +1,42 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 function Navbar() {
-	return (
-		<nav className="h-fit">
-			<div className="p-2">
-				<a href="/">
-					<img className="logo" src="logo.png" alt="logo" />
-				</a>
-			</div>
-			<div>
-				<ul>
-					<li>
-						<a href="/library">
-							Library<span></span>
-						</a>
-					</li>
-					<li>
-						<a href="/order">
-							Order<span></span>
-						</a>
-					</li>
-					<li>
-						<a href="/aboutus">
-							About us<span></span>
-						</a>
-					</li>
-					<li className="black">
-						<a
-							href="/login"
-							className="bg-black rounded-2xl hover:bg-slate-900"
-						>
-							Login<span></span>
-						</a>
-					</li>
-				</ul>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="h-fit">
+      <div className="p-2">
+        <NavLink to="/">
+          <img className="logo" src="logo.png" alt="logo" />
+        </NavLink>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <NavLink to="/library">
+              Library<span></span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/order">
+              Order<span></span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/aboutus">
+              About us<span></span>
+            </NavLink>
+          </li>
+          <li className="black">
+            <NavLink
+              to="/login"
+              className="bg-black rounded-2xl hover:bg-slate-900"
+            >
+              Login<span></span>
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
