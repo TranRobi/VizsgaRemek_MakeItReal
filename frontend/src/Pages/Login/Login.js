b86abd6565a481070edd82c72ff438cc6fc3d3c8
TranRobi <dtranduy@gmail.com>
2024-11-14 08:48:34 +0100

    login page

--- frontend/src/Pages/Login/Login.js
+++ frontend/src/Pages/Login/Login.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
 function Login() {
-  return (
-    <div>Login</div>
-  )
+	return (
+		<div className="flex justify-center h-4/5">
+			<div className="">
+				<h1>Login</h1>
+				<form>
+					<label>
+						Username:
+						<input type="text" name="username" />
+					</label>
+					<br />
+					<label>
+						Password:
+						<input type="password" name="password" />
+					</label>
+					<br />
+					<input type="submit" value="Submit" />
+				</form>
+				<p>
+					Don't have an account? <a href="/register">Register</a>
+				</p>
+			</div>
+		</div>
+	);
 }
 
-export default Login
+export default Login;
