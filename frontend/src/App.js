8a6209d5899b3146d02dcb467075fe671b5253cd
Tran Duy Dat <dtranduy2@gmail.com>
2024-11-14 16:25:19 +0100

    fixed some pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,7 @@ import "./App.css";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 import Home from "./Pages/Home/Home.js";
-import Login from "./Pages/Login/Login.js";
 import Library from "./Pages/Library/Library.js";
-import Register from "./Pages/Register/Register.js";
 import Order from "./Pages/Order/Order.js";
 import ContactUs from "./Pages/ContactUs/ContactUs.js";
 import AboutUs from "./Pages/AboutUs/AboutUs.js";
@@ -15,8 +13,6 @@ function App() {
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/library" element={<Library />} />
-					<Route path="/login" element={<Login />} />
-					<Route path="/register" element={<Register />} />
 					<Route path="/order" element={<Order />} />
 					<Route path="/contactus" element={<ContactUs />} />
 					<Route path="/aboutus" element={<AboutUs />} />
