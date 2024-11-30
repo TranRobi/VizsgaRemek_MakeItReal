def95fafd6fb08389ccd36b8b4ea1600e8551610
TranRobi <dtranduy@gmail.com>
2024-11-30 21:49:01 +0100

    added models

--- frontend/src/Pages/Prouducts/Prouduct.js
+++ frontend/src/Pages/Prouducts/Prouduct.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { useParams } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 
 import { NavLink } from "react-router-dom";
 
+import { Canvas } from "@react-three/fiber";
+import { Model } from "../../components/prodModel/Model1";
+import { OrbitControls } from "@react-three/drei";
 function Prouduct() {
 	const params = useParams();
 
 	return (
 		<>
 			<Navbar />
-			<div className="h-screen">
-				<h1>Product: {params.id}</h1>
-				<NavLink to={"/library"}>Go back</NavLink>
+			<div className="w-[500px] m-auto">
+				<div className="h-[500px] border-2 border-black">
+					<Canvas>
+						<Suspense fallback={null}>
+							<OrbitControls />
+							<ambientLight intensity={Math.PI / 2} />
+							<Model />
+						</Suspense>
+					</Canvas>
+				</div>
+				<NavLink to="/library">Go Back</NavLink>
 			</div>
 			<Footer />
 		</>
