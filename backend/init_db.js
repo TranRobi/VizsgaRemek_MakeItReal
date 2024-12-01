8c51395aadc7ac7a175a26ab277f1fa46c8e1c54
Zsolt Vadasz <zsolt_vadasz@protonmail.com>
2024-12-01 21:05:08 +0100

    backend/init_db.js: UML alapján táblák generálása

--- backend/init_db.js
+++ backend/init_db.js
@@ -30,7 +30,9 @@ query(`
     CREATE TABLE ${DB_NAME}.jobs (
         id INT UNSIGNED NOT NULL AUTO_INCREMENT,
         product_id INT UNSIGNED NULL,
+        address_id INT UNSIGNED NOT NULL,
         gcode_file_path VARCHAR(256) CHARACTER SET utf8 NOT NULL,
+        quantity INT UNSIGNED NOT NULL,
         material ENUM('PLA', 'PETG', 'ABS'),
         colour ENUM('Red', 'Green', 'Blue', 'Yellow', 'Black', 'White', 'Gray'),
         state ENUM('pending', 'in_production', 'shipped'),
@@ -38,5 +40,31 @@ query(`
     );
 `);
 
+console.log("'address' tábla létrehozása");
+query(`
+    CREATE TABLE ${DB_NAME}.address (
+        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+        country VARCHAR(128) CHARACTER SET utf8 NOT NULL,
+        county VARCHAR(256) CHARACTER SET utf8 NOT NULL,
+        city VARCHAR(256) CHARACTER SET utf8 NOT NULL,
+        number INT UNSIGNED NOT NULL,
+        postal_code INT UNSIGNED NOT NULL,
+        phone_number INT UNSIGNED  NOT NULL,
+        name VARCHAR(256) CHARACTER SET utf8 NOT NULL,
+        PRIMARY KEY(id)
+    );
+`);
+
+console.log("'users' tábla létrehozása");
+query(`
+    CREATE TABLE ${DB_NAME}.users (
+        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+        address_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+        email_address VARCHAR(256) CHARACTER SET utf8 NOT NULL,
+        display_name VARCHAR(64) CHARACTER_SET utf8 NOT NULL,
+        password VARCHAR(128) CHARACTER_SET utf8 NOT NULL,
+        PRIMARY KEY(id)
+    );
+`);
 
 connection.end();
