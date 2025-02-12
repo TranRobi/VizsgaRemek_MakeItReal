f531bacfbb5837195a75adfebc30528cd91de93f
TranRobi <dtranduy@gmail.com>
2025-02-12 09:36:15 +0100

    asd

--- backend/init_db.js
+++ backend/init_db.js
@@ -10,20 +10,20 @@ const DB_NAME = "makeitreal.db";
 console.log("Tiszta lappal kezdünk");
 // nem idempotens a függvény, szóval hibát dob, ha nincs létező db
 try {
-  unlinkSync(`./${DB_NAME}`);
+	unlinkSync(`./${DB_NAME}`);
 } catch (e) {}
 
 const db = new sqlite3.Database(`./${DB_NAME}`);
 console.log("SQLilte adatbázis létrehozva");
 
 const query = (q) =>
-  db.run(q, (err) => {
-    if (err) throw err;
-  });
+	db.run(q, (err) => {
+		if (err) throw err;
+	});
 
 db.serialize(() => {
-  console.log("'address' tábla létrehozása");
-  query(`
+	console.log("'address' tábla létrehozása");
+	query(`
         CREATE TABLE address (
             country VARCHAR(64) NOT NULL,
             county VARCHAR(128) NOT NULL,
@@ -35,8 +35,8 @@ db.serialize(() => {
         );
     `);
 
-  console.log("'jobs' tábla létrehozása");
-  query(`
+	console.log("'jobs' tábla létrehozása");
+	query(`
         CREATE TABLE jobs (
             product_id INT NULL,
             address_id INT NOT NULL,
@@ -49,8 +49,8 @@ db.serialize(() => {
         );
     `);
 
-  console.log("'users' tábla létrehozása");
-  query(`
+	console.log("'users' tábla létrehozása");
+	query(`
         CREATE TABLE users (
             address_id INT NULL,
             email_address VARCHAR(256) NOT NULL,
@@ -59,8 +59,8 @@ db.serialize(() => {
         );
     `);
 
-  console.log("'products' tábla létrehozása");
-  query(`
+	console.log("'products' tábla létrehozása");
+	query(`
         CREATE TABLE products (
             name VARCHAR(64) NOT NULL,
             description VARCHAR(512) NOT NULL,
@@ -71,26 +71,26 @@ db.serialize(() => {
         );
     `);
 
-  console.log("Dummy adatokkal feltöltés");
-  query(`
+	console.log("Dummy adatokkal feltöltés");
+	query(`
         INSERT INTO address(country, county, city, postal_code, street_number, phone_number, name) VALUES
         ('Magyarország', 'Pest', 'Budapest', 4000, 'Street utca 2', 36701234567, 'Vicc Elek'),
         ('Magyarország', 'Szabolcs-Szatmár-Bereg', 'Nyíregyháza', 4400, 'Street utca 3', 36701234567, 'Kriszh Advice');
     `);
-  let rowid = 0;
-  db.get(
-    `
+	let rowid = 0;
+	db.get(
+		`
         SELECT rowid FROM address WHERE name = 'Vicc Elek';
     `,
-    (err, row_id) => {
-      rowid = row_id;
-    }
-  );
-  query(`
+		(err, row_id) => {
+			rowid = row_id;
+		}
+	);
+	query(`
         INSERT INTO users VALUES(${rowid}, 'viccelek@citromail.hu', 'ViccElek', '888888');
     `);
 
-  query(`
+	query(`
         INSERT INTO products VALUES 
         ('Spártai pajzs', '300 egyike', '/dev/null', '/dev/null', 1),
         ('Spártai', '300', '/dev/null', '/dev/null', 2),
@@ -105,7 +105,27 @@ db.serialize(() => {
         ('Spártai', '300', '/dev/null', '/dev/null', 11),
         ('Spártai', '300', '/dev/null', '/dev/null', 12),
         ('Spártai', '300', '/dev/null', '/dev/null', 13),
-        ('Spártai', '300', '/dev/null', '/dev/null', 14);
+        ('Spártai', '300', '/dev/null', '/dev/null', 14),
+        ('Spártai', '300', '/dev/null', '/dev/null', 15),
+        ('Spártai', '300', '/dev/null', '/dev/null', 16),
+        ('Spártai', '300', '/dev/null', '/dev/null', 17),
+        ('Spártai', '300', '/dev/null', '/dev/null', 18),
+        ('Spártai', '300', '/dev/null', '/dev/null', 19),
+        ('Spártai', '300', '/dev/null', '/dev/null', 20),
+        ('Spártai', '300', '/dev/null', '/dev/null', 21),
+        ('Spártai', '300', '/dev/null', '/dev/null', 22),
+        ('Spártai', '300', '/dev/null', '/dev/null', 23),
+        ('Spártai', '300', '/dev/null', '/dev/null', 24),
+        ('Spártai', '300', '/dev/null', '/dev/null', 25),
+        ('Spártai', '300', '/dev/null', '/dev/null', 26),
+        ('Spártai', '300', '/dev/null', '/dev/null', 27),
+        ('Spártai', '300', '/dev/null', '/dev/null', 28),
+        ('Spártai', '300', '/dev/null', '/dev/null', 29),
+        ('Spártai', '300', '/dev/null', '/dev/null', 30),
+        ('Spártai', '300', '/dev/null', '/dev/null', 31),
+        ('Spártai', '300', '/dev/null', '/dev/null', 32),
+        ('Spártai', '300', '/dev/null', '/dev/null', 33),
+        ('Spártai', '300', '/dev/null', '/dev/null', 34);
     `);
 });
 
--- frontend/src/Pages/Library/Library.js
+++ frontend/src/Pages/Library/Library.js
@@ -9,58 +9,56 @@ import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 import axios from "axios";
 function Library() {
-  const [product, setProduct] = useState([]);
-  const [pageSize, setPageSize] = useState(10);
-  const [currentPage, setCurrentPage] = useState(1);
+	const [product, setProduct] = useState([]);
+	const [pageSize, setPageSize] = useState(10);
+	const [currentPage, setCurrentPage] = useState(1);
 
-  useEffect(() => {
-    axios
-      .get("/api/products")
-      .then((res) => {
-        setProduct(res.data);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }, []);
-  return (
-    <>
-      <Navbar />
-      <div className=" relative ">
-        <div className="cards justify-center h-[80vh] ">
-          {product
-            .map((prod, index) => {
-              return <Card key={index} prod={prod} />;
-            })
-            .slice(currentPage * pageSize - pageSize, currentPage * pageSize)}
-        </div>
-        <div className="bottom-0 flex justify-center items-center">
-          <IconButton>
-            <ArrowBackIcon
-              onClick={() => {
-                if (currentPage != 1) {
-                  setCurrentPage(currentPage - 1);
-                }
-              }}
-              fontSize="large"
-            />
-          </IconButton>
-          <span>Page {currentPage}</span>
-          <IconButton>
-            <ArrowForwardIcon
-              onClick={() => {
-                if (currentPage * pageSize < product.length) {
-                  setCurrentPage(currentPage + 1);
-                }
-              }}
-              fontSize="large"
-            />
-          </IconButton>
-        </div>
-      </div>
-      <Footer />
-    </>
-  );
+	useEffect(() => {
+		axios
+			.get("/api/products")
+			.then((res) => {
+				setProduct(res.data);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	}, []);
+	return (
+		<>
+			<Navbar />
+			<div>
+				<div className="cards justify-center h-[90vh] ">
+					{product
+						.map((prod, index) => {
+							return <Card key={index} prod={prod} />;
+						})
+						.slice(currentPage * pageSize - pageSize, currentPage * pageSize)}
+				</div>
+			</div>
+			<div className="flex justify-center items-center">
+				<IconButton
+					onClick={() => {
+						if (currentPage != 1) {
+							setCurrentPage(currentPage - 1);
+						}
+					}}
+				>
+					<ArrowBackIcon fontSize="large" />
+				</IconButton>
+				<span>Page {currentPage}</span>
+				<IconButton
+					onClick={() => {
+						if (currentPage * pageSize < product.length) {
+							setCurrentPage(currentPage + 1);
+						}
+					}}
+				>
+					<ArrowForwardIcon fontSize="large" />
+				</IconButton>
+			</div>
+			<Footer />
+		</>
+	);
 }
 
 export default Library;
