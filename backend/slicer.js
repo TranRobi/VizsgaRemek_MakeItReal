4627b44e89e02a4df7e2a7d207ef17b2a70cafc0
Zsolt Vadasz <zsolt_vadasz@protonmail.com>
2025-03-22 18:44:19 +0100

    backend: slicer.js fejlesztések

--- backend/slicer.js
+++ backend/slicer.js
@@ -10,14 +10,39 @@ export const is_slicer_installed = () => {
     } else {
         try {
             execSync('prusa-slicer --help');
+            return true;
         } catch(e) {
             return false;
         }
-        return true;
+    }
+};
+
+export const is_stl_thumb_installed = () => {
+    if (os.type() === 'Windows_NT') {
+        try {
+            execSync('stl-thumb.exe --version');
+            return true;
+        } catch (e) {
+            return false;
+        }
+    } else {
+        try {
+            execSync('stl-thumb --version');
+            return true;
+        } catch (e) {
+            return false;
+        }
     }
 };
 
 export const slicer = () => os.type() === 'Windows_NT' ? '' : execSync('which prusa-slicer').toString();
 
 export const slice_stl_to_gcode = (stl_path, gcode_path) => {
+    // TODO check success
+    execSync(`${slicer()}
+        --output-file ${gcode_path}
+        --slice ${stl_path}
+        --pad-around-object
+        --supports-enable`);
+    return true;
 };
