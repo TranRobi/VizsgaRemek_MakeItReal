4d6b2d569075dfe3074ab05c01141876a3acc850
Mester Máté <mestermate6@gmail.com>
2025-03-26 18:54:09 +0100

    styled home page

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -19,10 +19,10 @@ function Home() {
       <div className="w-4/5 mx-auto py-12">
         <section className=" p-8 rounded-lg mb-12 flex flex-col md:flex-row-reverse items-center gap-8">
           <div className="md:w-1/2 text-center">
-            <h1 className="sm:text-5xl md:7xl text-4xl font-bold text-[#950101]">
+            <h1 className="sm:text-5xl md:7xl text-4xl font-bold text-white">
               Make it real
             </h1>
-            <h3 className="text-2xl text-[#5e0505] mt-4">
+            <h3 className="text-2xl text-white mt-4">
               {" "}
               Dream it and we will make it into reality
             </h3>
@@ -37,10 +37,10 @@ function Home() {
         {/* About Us Section */}
         <section className=" p-8 rounded-lg mb-12 flex flex-col md:flex-row items-center gap-8">
           <div className="md:w-1/2">
-            <h1 className="text-4xl font-semibold text-[#333333]">About us</h1>
-            <h2 className="text-2xl text-[#5A738E] mt-2">Who are we?</h2>
-            <h2 className="text-2xl text-[#5A738E]">How did we start?</h2>
-            <h2 className="text-2xl text-[#5A738E]">What are we doing?</h2>
+            <h1 className="text-5xl font-semibold text-white">About us</h1>
+            <h2 className="text-2xl text-white mt-2">Who are we?</h2>
+            <h2 className="text-2xl text-white">How did we start?</h2>
+            <h2 className="text-2xl text-white">What are we doing?</h2>
             <NavLink
               to="/aboutus"
               className="mt-4 inline-block bg-[#333333] text-white px-6 py-3 rounded-lg font-medium hover:bg-[#000000] transition"
@@ -58,10 +58,10 @@ function Home() {
         {/* Contact Section */}
         <section className=" p-8 rounded-lg shadow-lg mb-12 flex flex-col md:flex-row-reverse items-center gap-8">
           <div className="md:w-1/2">
-            <h1 className="text-4xl font-semibold text-[#333333]">
+            <h1 className="text-5xl font-semibold text-white">
               Contact us
             </h1>
-            <h3 className="text-xl text-[#5A738E] mt-2">
+            <h3 className="text-xl text-white mt-2">
               For help or questions, please contact us
             </h3>
             <NavLink
@@ -79,34 +79,50 @@ function Home() {
         </section>
 
         {/* Library Section */}
-        <section className=" p-8 rounded-lg shadow-lg text-center mb-12">
-          <h1 className="text-4xl font-semibold text-[#333333]">
-            Explore More
-          </h1>
-          <h3 className="text-xl text-[#5A738E] mt-2">
-            For more products click on the button below
-          </h3>
-          <NavLink
-            to="/library"
-            className="mt-4 inline-block bg-[#333333] text-white px-6 py-3 rounded-lg font-medium hover:bg-[#000000] transition"
-          >
-            Visit Library <ArrowForwardIcon className="ml-2" />
-          </NavLink>
+        <section className="p-8 rounded-lg shadow-lg mb-12 flex flex-col md:flex-row items-center gap-8">
+          <div className="md:w-1/2">
+            <h1 className="text-4xl font-semibold text-white">
+              Explore More
+            </h1>
+            <h3 className="text-xl text-white mt-2">
+              For more products click on the button below
+            </h3>
+            <NavLink
+              to="/library"
+              className="mt-4 inline-block bg-[#333333] text-white px-6 py-3 rounded-lg font-medium hover:bg-[#000000] transition"
+            >
+              Visit Library <ArrowForwardIcon className="ml-2" />
+            </NavLink>
+          </div>
+          <img
+            src="arrow.PNG"
+            alt="Arrow"
+            className="w-full md:w-1/2 rounded-lg shadow-lg"
+          />
         </section>
 
         {/* Order Section */}
-        <section className=" p-8 rounded-lg shadow-lg text-center mb-12">
-          <h1 className="text-4xl font-semibold text-[#333333]">Order now</h1>
-          <h3 className="text-xl text-[#5A738E] mt-2">
-            This is for clients with ideas in mind that need to be modeled and
-            made into reality
-          </h3>
-          <NavLink
-            to="/order"
-            className="mt-4 inline-block bg-[#333333] text-white px-6 py-3 rounded-lg font-medium hover:bg-[#000000] transition"
-          >
-            Order now <LocalShippingIcon className="ml-2" />
-          </NavLink>
+        <section className=" p-8 rounded-lg shadow-lg mb-12 flex flex-col md:flex-row-reverse items-center gap-8">
+          <div className="md:w-1/2">
+            <h1 className="text-5xl font-semibold text-white">
+              Order now
+            </h1>
+            <h3 className="text-xl text-white mt-2">
+              This is for clients with ideas in mind that need to be modeled and
+              made into reality
+            </h3>
+            <NavLink
+              to="/contactus"
+              className="mt-4 inline-block bg-[#333333] text-white px-6 py-3 rounded-lg font-medium hover:bg-[#000000] transition"
+            >
+              Contact us now <ArrowForwardIcon className="ml-2" />
+            </NavLink>
+          </div>
+          <img
+            src="delivery.gif"
+            alt="Contact us"
+            className="w-full md:w-1/2 rounded-lg shadow-lg"
+          />
         </section>
       </div>
       <Footer />
