7b9eb7308fe02f0f6e92b5535489e2f569e28203
Zsolt Vadasz <zsolt_vadasz@protonmail.com>
2025-04-06 16:44:22 +0200

    backend: Konstansok hardcode-olt értékek helyett

--- backend/config.js
+++ backend/config.js
@@ -1,10 +1,34 @@
 'use strict';
 
-const CONFIG = {
+export const CONFIG = {
     DB_NAME: 'makeitreal.db',
     HASH_ITERATIONS: 128,
     SALT_SIZE: 32,
     PASSWORD_HASH_SIZE: 128
+};
+
+/* konstansok query-khez */
+export const JOB_MATERIALS = {
+    PLA: 'PLA',
+    PETG: 'PETG',
+    ABS: 'ABS',
+};
+
+export const JOB_STATES = {
+    PENDING: 'pending',
+    IN_PRODUCTION: 'in_production',
+    SHIPPED: 'shipped',
+    DONE: 'done',
+};
+
+export const JOB_COLOURS = {
+    RED: 'Red',
+    GREEN: 'Green',
+    BLUE: 'Blue',
+    YELLOW: 'Yellow',
+    BLACK: 'Black',
+    WHITE: 'White',
+    GRAY: 'Gray',
 };
 
 export default CONFIG;
